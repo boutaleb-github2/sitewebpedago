5598ca481523b32b8c18b32af5e11c240a86a3b0
boutaleb-github2 <boutaleb.habib.github2@gmail.com>
2025-11-20 19:29:43 +0100

    add express-session

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 const express = require('express');
+const session = require('express-session')
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Pour servir le CSS et autres fichiers statiques
+// Pour lire les données des formulaires (POST)
+app.use(express.urlencoded({ extended: true }));
+
+// Pour servir le CSS et autres fichiers statiques comme des pdf par exemple! 
 app.use(express.static('public'));
+
+// Configuration des sessions
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'dev-secret',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+// le middleware requireAuth
+function requireAuth(req, res, next) {
+  if (req.session && req.session.isAuth) {
+    return next(); // OK, il est connecté → on continue vers la route
+  }
+  // Pas connecté → on renvoie vers la page de login
+  res.redirect('/login');
+}
 
 // Petite fonction pour générer une page HTML avec un layout commun
 function renderPage(title, content) {
@@ -25,6 +49,7 @@ function renderPage(title, content) {
           <a href="/cours/dielectricandmagneticmaterials">Cours matériaux diélectriques et magnétiques</a>
           <a href="/cours/opensourcesphysique">opensourcesphysique</a>
           <a href="/exercices">Exercices</a>
+           <a href="/logout">Déconnexion</a>
         </nav>
       </header>
 
@@ -54,14 +79,76 @@ app.get('/', (req, res) => {
   );
 });
 
-// Cours JavaScript
-app.get('/cours/quantummecanics', (req, res) => {
+////------------------
+
+// Page de login (formulaire)
+app.get('/login', (req, res) => {
+  res.send(
+    renderPage(
+      'Connexion',
+      `
+      <h2>Connexion</h2>
+      <form method="POST" action="/login">
+        <div>
+          <label>Utilisateur :</label>
+          <input type="text" name="username" required>
+        </div>
+        <div>
+          <label>Mot de passe :</label>
+          <input type="password" name="password" required>
+        </div>
+        <button type="submit">Se connecter</button>
+      </form>
+      `
+    )
+  );
+});
+
+// Traitement du login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const adminUser = process.env.ADMIN_USER;
+  const adminPass = process.env.ADMIN_PASS;
+
+  if (username === adminUser && password === adminPass) {
+    // On marque la session comme "authentifiée"
+    req.session.isAuth = true;
+    return res.redirect('/cours/quantummecanics');
+  }
+
+  // Mauvais identifiants
+  res.send(
+    renderPage(
+      'Connexion',
+      `
+      <h2>Connexion</h2>
+      <p style="color:red;">Utilisateur ou mot de passe incorrect</p>
+      <a href="/login">Réessayer</a>
+      `
+    )
+  );
+});
+
+// Déconnexion
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
+
+
+////------------------
+
+// Cours mecanique quantique
+app.get('/cours/quantummecanics', requireAuth,(req, res) => {
   res.send(
     renderPage(
       'Cours JavaScript',
       `
       <h2>Cours Mécanique quantique</h2>
       <p>Mécanique quantique.</p>
+      <a href="/logout">Déconnexion</a>
       <h3>Cours en pptx :</h3>
 <iframe 
   src="https://view.officeapps.live.com/op/embed.aspx?src=https://sitewebpedago-habib-boutaleb-f23d2b5f9cdd.herokuapp.com/quantummecanics.pptx"
